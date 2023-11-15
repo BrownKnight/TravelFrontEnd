cb17e96453de140d66c5dffb5d9d4aedbc07206f
BrownKnight <amanrk@hotmail.co.uk>
2023-11-15 19:35:38 +0000

    Initial Commit

---
+++ FrontEnd/FrontEndApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrontEndApp.swift
+//  FrontEnd
+//
+//  Created by Amanvir Dhoot on 15/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FrontEndApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
